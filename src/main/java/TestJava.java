bf36091122cb3cffd519a0030c15378cd382e6d4
12244 <proline.impex@gmail.com>
2021-11-01 20:38:50 -0500

    firt Commit for JavaTest Project

---
+++ src/main/java/TestJava.java
@@ -0,0 +1,23 @@
+
+
+/**
+ *
+ * @author 12244
+ */
+public class TestJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        String hungry = "yes";
+        
+        if (hungry == "yes")
+                System.out.println("Eat Something...");
+        else {
+                System.out.println("Do work....");
+    }
+    }
+    
+}
